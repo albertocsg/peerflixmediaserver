61822b88f28d08298dfa428e17bb31d76a218576
Alberto C. Sánchez <albertocsg@gmail.com>
2014-08-12 18:53:13 +0200

    v0.1.0 -DivxTotal added.

--- src/main/java/controller/HttpServerImp.java
+++ src/main/java/controller/HttpServerImp.java
@@ -36,7 +36,7 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 
 	@Autowired
 	private IDocumentsOper documentsOperDivxTotal;
-	
+
 	@Autowired
 	private IRunPeerflix runPeerflix;
 
@@ -48,10 +48,10 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 	private String searchPage = "";
 	private int typePage = 0;
 	private int elementsPerPage = 16;
-	
+	private SOURCE currentSource = null;
+
 	enum SOURCE {
-		ELITETORRENT,
-		DIVXTOTAL
+		ELITETORRENT, DIVXTOTAL
 	}
 
 	/**
@@ -85,44 +85,86 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 		OutputStream out = null;
 		String response = null;
 
-		if (exchange.getRequestURI().getPath().equals("/list")) {
+		if (exchange.getRequestURI().getPath().equals("/") ||
+				exchange.getRequestURI().getPath().equals("/list") ||
+				exchange.getRequestURI().getPath().equals("/cat_0_1")) {
 			numPage = 0;
 			typePage = 0;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/estrenos")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_1_1")) {
 			numPage = 0;
 			typePage = 1;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/peliculas")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_2_1")) {
 			numPage = 0;
 			typePage = 2;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/hdrip")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_3_1")) {
 			numPage = 0;
 			typePage = 3;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/microhd")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_4_1")) {
 			numPage = 0;
 			typePage = 4;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/series")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_5_1")) {
 			numPage = 0;
 			typePage = 5;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/docusytv")) {
+		} else if (exchange.getRequestURI().getPath().equals("/cat_6_1")) {
 			numPage = 0;
 			typePage = 6;
 			response = tryList(SOURCE.ELITETORRENT);
-		} else if (exchange.getRequestURI().getPath().equals("/search")) {
+		} else if (exchange.getRequestURI().getPath().equals("/search_1")) {
 			numPage = 0;
 			typePage = 10;
 			searchPage = exchange.getRequestURI().getQuery();
-			response = trySearch();
-		} else if (exchange.getRequestURI().getPath().equals("/searchpch")) {
+			response = trySearch(SOURCE.ELITETORRENT);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_0_2")) {
+			numPage = 0;
+			typePage = 0;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_1_2")) {
+			numPage = 0;
+			typePage = 1;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_2_2")) {
+			numPage = 0;
+			typePage = 2;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_3_2")) {
+			numPage = 0;
+			typePage = 3;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_4_2")) {
+			numPage = 0;
+			typePage = 4;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_5_2")) {
+			numPage = 0;
+			typePage = 5;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_6_2")) {
+			numPage = 0;
+			typePage = 6;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_7_2")) {
+			numPage = 0;
+			typePage = 7;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_8_2")) {
+			numPage = 0;
+			typePage = 8;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/cat_9_2")) {
+			numPage = 0;
+			typePage = 8;
+			response = tryList(SOURCE.DIVXTOTAL);
+		} else if (exchange.getRequestURI().getPath().equals("/search_2")) {
 			numPage = 0;
 			typePage = 10;
 			searchPage = exchange.getRequestURI().getQuery();
-			response = trySearch();
+			response = trySearch(SOURCE.DIVXTOTAL);
 		} else if (exchange.getRequestURI().getPath().equals("/detail")) {
 			response = tryDetail();
 		} else if (exchange.getRequestURI().getPath().equals("/torrent")) {
@@ -149,49 +191,49 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 		List<Ficha> fichasTmp;
 		String page;
 		
+		currentSource = source;
+		IDocumentsOper docOper = getDocumentsOper(source);
+
 		// If it's the first page, the refill the list of fichas
 		if (numPage == 0) {
 			fichas = new ArrayList<Ficha>();
 			for (int i = 1; i <= 10; i++) {
 				fichasTmp = null;
-				switch (source) {
-					case ELITETORRENT:
-						page = documentsOperElite.getPageURL(typePage, i);
-						fichasTmp = documentsOperElite.processPage(page);
-						break;
-					case DIVXTOTAL:
-						page = documentsOperDivxTotal.getPageURL(typePage, i);
-						fichasTmp = documentsOperDivxTotal.processPage(page);
-						break;
-				}
+				page = docOper.getPageURL(typePage, i);
+				fichasTmp = docOper.processPage(page);
 				fichas.addAll(fichasTmp);
 			}
 			numPage = 1;
 		}
-		
+
 		fichasTmp = getSelectedElementsByPage();
-		
+
 		response.append(prepareList(fichasTmp));
 
 		return response.toString();
 	}
 
-	private String trySearch() {
+	private String trySearch(SOURCE source) {
 		StringBuilder response = new StringBuilder();
 		List<Ficha> fichasTmp;
+
+		currentSource = source;
 		
+		IDocumentsOper docOper = getDocumentsOper(source);
+
 		// If it's the first page, the refill the list of fichas
-		if (numPage == 1) {
+		if (numPage == 0) {
 			fichas = new ArrayList<Ficha>();
 			for (int i = 1; i <= 10; i++) {
-				String page = documentsOperElite.getPageSearch(searchPage, i);
-				fichasTmp = documentsOperElite.processPage(page);
+				String page = docOper.getPageSearch(searchPage, i);
+				fichasTmp = docOper.processPage(page);
 				fichas.addAll(fichasTmp);
 			}
+			numPage = 1;
 		}
 
 		fichasTmp = getSelectedElementsByPage();
-		
+
 		response.append(prepareList(fichasTmp));
 
 		return response.toString();
@@ -205,9 +247,11 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 		StringBuilder response = new StringBuilder();
 		int i = Integer.valueOf(index);
 		i = i + ((numPage - 1) * elementsPerPage);
+		
+		IDocumentsOper docOper = getDocumentsOper(currentSource);
 
 		if (fichas.get(i).getTorrent() == null) {
-			documentsOperElite.getTorrent(fichas.get(i));
+			docOper.getTorrent(fichas.get(i));
 		}
 
 		runPeerflix.run(fichas.get(i));
@@ -270,6 +314,7 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 	private String getHtmlFooter() {
 		StringBuilder footer = new StringBuilder();
 		footer.append("</body>\n</html>\n");
+		footer.append("                                             ");
 		return footer.toString();
 
 	}
@@ -277,17 +322,30 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 	private String getCategories() {
 		StringBuilder response = new StringBuilder();
 
-		response.append("<a href=\"./list\">Todos</a><br>\n");
-		response.append("<a href=\"./estrenos\">Estrenos</a><br>\n");
-		response.append("<a href=\"./peliculas\">Peliculas</a><br>\n");
-		response.append("<a href=\"./hdrip\">HDRIP</a><br>\n");
-		response.append("<a href=\"./microhd\">MicroHD</a><br>\n");
-		response.append("<a href=\"./series\">Series</a><br>\n");
-		response.append("<a href=\"./docusytv\">Documentales y TV</a><br>\n");
-		response.append("<a href=\"./search?banshee\">Banshee</a><br>\n");
-		response.append("<a href=\"./search?big+bang\">Big Bang Theory</a><br>\n");
+		response.append("ELITETORRENT: ");
+		response.append("<a href=\"./cat_0_1\">Todos</a> | \n");
+		response.append("<a href=\"./cat_1_1\">Estrenos</a> | \n");
+		response.append("<a href=\"./cat_2_1\">Peliculas</a> | \n");
+		response.append("<a href=\"./cat_3_1\">HDRIP</a> | \n");
+		response.append("<a href=\"./cat_4_1\">MicroHD</a> | \n");
+		response.append("<a href=\"./cat_5_1\">Series</a> | \n");
+		response.append("<a href=\"./cat_6_1\">Documentales y TV</a> | \n");
+		response.append("<a href=\"./search_1?banshee\">Banshee</a> | \n");
+		response.append("<a href=\"./search_1?big+bang+theory\">Big Bang Theory</a>\n");
 		response.append("<br>\n");
-		
+		response.append("DIVXTOTAL: ");
+		response.append("<a href=\"./cat_0_2\">Todos</a> | \n");
+		response.append("<a href=\"./cat_1_2\">Animación</a> | \n");
+		response.append("<a href=\"./cat_2_2\">Acción</a> | \n");
+		response.append("<a href=\"./cat_3_2\">Aventura</a> | \n");
+		response.append("<a href=\"./cat_4_2\">Comedia</a> | \n");
+		response.append("<a href=\"./cat_5_2\">Drama</a> | \n");
+		response.append("<a href=\"./cat_6_2\">Sci-fi</a> | \n");
+		response.append("<a href=\"./cat_7_2\">Terror</a> | \n");
+		response.append("<a href=\"./cat_8_2\">Thriller</a> | \n");
+		response.append("<a href=\"./cat_9_2\">Españolas</a>\n");
+		response.append("<br><br>\n");
+
 		return response.toString();
 	}
 
@@ -308,7 +366,7 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 		response.append(numPage);
 		response.append(" | <a href=\"./back\">Anterior</a><br><br>");
 		response.append(getCategories());
-	
+
 		if (fichas != null) {
 			response.append("<table>");
 			int col = 0;
@@ -320,7 +378,7 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 					}
 					response.append("<td><img src=\"")
 							.append(ficha.getImagen())
-							.append("\" border=\"0\"><br>\n")
+							.append("\" border=\"0\" width=150 height=222><br>\n")
 							.append("<a href=\"./torrent?").append(i)
 							.append("\">").append(ficha.getNombre())
 							.append("</a>").append("<td>\n");
@@ -338,40 +396,57 @@ public class HttpServerImp implements IHttpServer, HttpHandler {
 
 		return response.toString();
 	}
-	
+
 	private String tryNext() {
 		numPage++;
 		if (typePage == 10) {
-			return trySearch();
+			return trySearch(currentSource);
 		} else {
-			return tryList(SOURCE.ELITETORRENT);
+			return tryList(currentSource);
 		}
 	}
-	
+
 	private String tryBack() {
 		numPage--;
 		if (numPage < 1) {
 			numPage = 1;
 		}
 		if (typePage == 10) {
-			return trySearch();
+			return trySearch(currentSource);
 		} else {
-			return tryList(SOURCE.ELITETORRENT);
+			return tryList(currentSource);
 		}
 	}
-	
+
 	private List<Ficha> getSelectedElementsByPage() {
 		int firstElement = ((numPage - 1) * elementsPerPage) + 1;
 		List<Ficha> selection = new ArrayList<Ficha>();
-		
-		for (int i=0; i<elementsPerPage; i++) {
-			if (i+firstElement > fichas.size()) {
+
+		for (int i = 0; i < elementsPerPage; i++) {
+			if (i + firstElement > fichas.size()) {
 				break;
 			}
-			selection.add(fichas.get(firstElement+i-1));
+			selection.add(fichas.get(firstElement + i - 1));
+		}
+
+		return selection;
+	}
+	
+	private IDocumentsOper getDocumentsOper(SOURCE source) {
+		IDocumentsOper docOper = documentsOperElite;
+		
+		switch (source) {
+			case ELITETORRENT:
+				docOper = documentsOperElite;
+				break;
+	
+			case DIVXTOTAL:
+				docOper = documentsOperDivxTotal;
+				break;
 		}
 		
-		return selection;
+		return docOper;
+		
 	}
 
 }
--- src/main/java/oper/webpage/DocumentsOperDivxTotal.java
+++ src/main/java/oper/webpage/DocumentsOperDivxTotal.java
@@ -21,76 +21,62 @@ import org.springframework.stereotype.Component;
 public class DocumentsOperDivxTotal implements IDocumentsOper {
 
 	public String getPageURL(int typeDoc, int page) {
-		StringBuilder document = new StringBuilder();
-		try {
-			
-			URL url = null;
-			
-			switch (typeDoc) {
-			
-				case 1:	// Animación
-					url = new URL("http://www.divxtotal.com/peliculas/genero-114/pagina/" + page + "/");
-					break;
-					
-				case 2: // Acción
-					url = new URL("http://www.divxtotal.com/peliculas/genero-101/pagina/" + page + "/");
-					break;
-					
-				case 3: // Aventura
-					url = new URL("http://www.divxtotal.com/peliculas/genero-103/pagina/" + page + "/");
-					break;
-					
-				case 4: // Comedia
-					url = new URL("http://www.divxtotal.com/peliculas/genero-104/pagina/" + page + "/");
-					break;
-					
-				case 5: // Drama
-					url = new URL("http://www.divxtotal.com/peliculas/genero-106/pagina/" + page + "/");
-					break;
-					
-				case 6:	// Sci-fi
-					url = new URL("http://www.divxtotal.com/peliculas/genero-107/pagina/" + page + "/");
-					break;
-					
-				case 7:	// Terror
-					url = new URL("http://www.divxtotal.com/peliculas/genero-108/pagina/" + page + "/");
-					break;
-					
-				case 8:	// Thriller
-					url = new URL("http://www.divxtotal.com/peliculas/genero-109/pagina/" + page + "/");
-					break;
-					
-				case 9:	// Españolas
-					url = new URL("http://www.divxtotal.com/peliculas/genero-112/pagina/" + page + "/");
-					break;
-					
-				default:	// all
-					url = new URL("http://www.divxtotal.com/peliculas/pagina/" + page + "/");
-					break;
-			
-			}
-			
-			URLConnection conn = url.openConnection();
-			BufferedReader entrada = new BufferedReader( new InputStreamReader(conn.getInputStream()));
-			
-			String linea;
-			
-			while ((linea = entrada.readLine()) != null) {
-				document.append(linea);
-			}
+		String url;
+		switch (typeDoc) {
 
-			entrada.close();
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		case 1: // Animación
+			url = "http://www.divxtotal.com/peliculas/genero-114/pagina/"
+					+ page + "/";
+			break;
+
+		case 2: // Acción
+			url = "http://www.divxtotal.com/peliculas/genero-101/pagina/"
+					+ page + "/";
+			break;
+
+		case 3: // Aventura
+			url = "http://www.divxtotal.com/peliculas/genero-103/pagina/"
+					+ page + "/";
+			break;
+
+		case 4: // Comedia
+			url = "http://www.divxtotal.com/peliculas/genero-104/pagina/"
+					+ page + "/";
+			break;
+
+		case 5: // Drama
+			url = "http://www.divxtotal.com/peliculas/genero-106/pagina/"
+					+ page + "/";
+			break;
+
+		case 6: // Sci-fi
+			url = "http://www.divxtotal.com/peliculas/genero-107/pagina/"
+					+ page + "/";
+			break;
+
+		case 7: // Terror
+			url = "http://www.divxtotal.com/peliculas/genero-108/pagina/"
+					+ page + "/";
+			break;
+
+		case 8: // Thriller
+			url = "http://www.divxtotal.com/peliculas/genero-109/pagina/"
+					+ page + "/";
+			break;
+
+		case 9: // Españolas
+			url = "http://www.divxtotal.com/peliculas/genero-112/pagina/"
+					+ page + "/";
+			break;
+
+		default: // all
+			url = "http://www.divxtotal.com/peliculas/pagina/" + page + "/";
+			break;
+
 		}
-		
-		return document.toString();
-		
+
+		return getPage(url);
+
 	}
 
 	public List<Ficha> processPage(String page) {
@@ -100,60 +86,55 @@ public class DocumentsOperDivxTotal implements IDocumentsOper {
 		for (Element e : elements) {
 			Ficha ficha = new Ficha();
 			ficha.setNombre(e.getElementsByTag("a").get(0).text());
-			//ficha.setImagen(e.getElementsByTag("img").get(0).attr("abs:src"));
-			ficha.setUrl(e.getElementsByTag("a").get(0).attr("abs:href"));
+			ficha.setUrl("http://www.divxtotal.com" + e.getElementsByTag("a").get(0).attr("href"));
+
+			String image = e.getElementsByTag("a").get(0).attr("href");
+			image = image.replaceAll("/peliculas/torrent/", "");
+			image = image.replace("/", ".");
+			image = "http://www.divxtotal.com/torrents_img/t" + image + "jpg";
+			ficha.setImagen(image);
+
 			fichas.add(ficha);
 		}
 		return fichas;
 	}
-	
+
 	public void getTorrent(Ficha ficha) {
-		if (ficha == null) {
-			return;
-		}
-		
-		try {
-			
-			Document doc = Jsoup.parse(new URL(ficha.getUrl()), 10000);
-			String magnet = doc.getElementsByClass("enlace_torrent").get(1).attr("href");
-			String details = doc.getElementsByClass("detalles").get(0).html();
-			
-			ficha.setTorrent(magnet);
-			ficha.setDetails(details);
-			
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		// Check if ficha is not null and if has url.
+		if (ficha != null & ficha.getUrl() != null && !ficha.getUrl().equals("")) {
+
+			String page = getPage(ficha.getUrl());
+
+			Document doc = Jsoup.parse(page);
+			Elements elements = doc.getElementsByClass("ficha_link_det");
+			ficha.setTorrent("http://www.divxtotal.com" + elements.get(0).getElementsByTag("a").get(0)
+					.attr("href"));
 		}
 	}
 
 	public String getPageSearch(String search, int page) {
 		StringBuilder document = new StringBuilder();
 		try {
-			
+
 			URL url = null;
 			String searchText = search;
 			searchText.replace(" ", "+");
-			
-			url = new URL("http://www.elitetorrent.net/busqueda/" + searchText + "/pag:" + page);
-			
+
+			url = new URL("http://www.divxtotal.com/buscar.php?busqueda=" + searchText
+					+ "&pagina=" + page);
+
 			URLConnection conn = url.openConnection();
-			BufferedReader entrada = new BufferedReader( new InputStreamReader(conn.getInputStream()));
-			
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+
 			String linea;
-			
+
 			while ((linea = entrada.readLine()) != null) {
 				document.append(linea);
 			}
 
 			entrada.close();
-			
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -161,9 +142,65 @@ public class DocumentsOperDivxTotal implements IDocumentsOper {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return document.toString();
-		
+
 	}
-	
+
+	/**
+	 * Get the html of the given page.
+	 * @param page URL
+	 * @return String with the html
+	 */
+	private String getPage(String page) {
+		StringBuilder document = new StringBuilder();
+		try {
+
+			URL url = new URL(page);
+			;
+
+			URLConnection conn = url.openConnection();
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+
+			String linea;
+
+			while ((linea = entrada.readLine()) != null) {
+				document.append(linea);
+			}
+
+			entrada.close();
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return document.toString();
+
+	}
+
+	/**
+	 * Get and update the image and torrent uri of the given ficha
+	 * @param ficha
+	 */
+	private void getImageAndTorrent(Ficha ficha) {
+		// Check if ficha is not null and if has url.
+		if (ficha != null & ficha.getUrl() != null && !ficha.getUrl().equals("")) {
+
+			String page = getPage(ficha.getUrl());
+
+			Document doc = Jsoup.parse(page);
+			Elements elements = doc.getElementsByClass("ficha_img");
+			ficha.setImagen("http://www.divxtotal.com" + elements.get(0).getElementsByTag("img").get(0)
+					.attr("src"));
+			elements = doc.getElementsByClass("ficha_link_det");
+			ficha.setTorrent("http://www.divxtotal.com" + elements.get(0).getElementsByTag("a").get(0)
+					.attr("href"));
+		}
+	}
+
 }
